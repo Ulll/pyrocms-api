a22b91020bc4ff4257bf1cf46d6e5f2499665138
Jo <mod@heiljo.com>
2018-07-04 15:21:43 +0800

    add router

---
+++ src/Providers/PyrocmsapiServiceProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Pyrocmsapi\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class PyrocmsapiServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'Pyrocmsapi\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapApiRoutes();
+        //
+    }
+
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::group(
+            [
+                'middleware' => 'api',
+                'namespace'  => $this->namespace,
+                'prefix'     => 'api',
+            ],
+            function ($router) {
+                require realpath(__DIR__ . '/../Routes/api.php');
+            }
+        );
+    }
+}
